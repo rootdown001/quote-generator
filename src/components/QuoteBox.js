7ec1323a517bf682ce8917e471a830d1738b0c88
Lance Anderson <lance.a.585@gmail.com>
2023-01-14 18:59:33 -0800

    Edit - Edit buttons

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -10,7 +10,7 @@ export default function QuoteBox() {
             <div className="quote-author">~ <span id="author">Lance Anderson</span>
             </div>
             <div className="button-row">
-                <button className="btn" id="tweet-quote"><i class="fa fa-twitter"></i></button>
+                <button className="btn" id="tweet-quote" ><a href="https://twitter.com/intent/tweet"><i class="fa fa-twitter"></i></a></button>
                 <button className="btn" id="new-quote">New Quote</button>
             </div>
         </div>
